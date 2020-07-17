5ce8d5628cb48ced70b45c9df7cb4ac7011e7149
Duc <vanduc2514@gmail.com>
2020-07-18 03:52:18 +0700

    Init Project

---
+++ src/test/java/com/example/vndreamers/DreamersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vndreamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamersBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
