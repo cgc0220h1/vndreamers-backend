1f71d61416c867545b57561b991c9c38b0e816c3
Duc <vanduc2514@gmail.com>
2020-07-18 16:51:40 +0700

    Thêm User và Role Entity

---
+++ src/main/java/com/example/vndreamers/models/Role.java
@@ -0,0 +1,41 @@
+package com.example.vndreamers.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name = "role", schema = "vndreamers")
+@Data
+public class Role {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Basic
+    @Column(name = "role_name", nullable = false, length = 10)
+    private String roleName;
+
+    @Basic
+    @Column(name = "created_date", nullable = false)
+    private Timestamp createdDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+
+    @ManyToMany
+    @JoinTable(
+            name = "role_user",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+}
