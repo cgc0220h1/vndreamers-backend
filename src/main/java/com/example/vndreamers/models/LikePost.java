e55f0dc767a5de52d00fb354dc2be0840adc38b4
Duc <vanduc2514@gmail.com>
2020-07-18 16:06:41 +0700

    Thêm Like Post và Permission Entity

---
+++ src/main/java/com/example/vndreamers/models/LikePost.java
@@ -0,0 +1,36 @@
+package com.example.vndreamers.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "like_post", schema = "vndreamers")
+@Data
+public class LikePost {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Basic
+    @Column(name = "status", nullable = false)
+    private int status;
+
+    @Basic
+    @Column(name = "created_date", nullable = false)
+    private Timestamp createdDate;
+
+    @Basic
+    @Column(name = "modify_date", nullable = false)
+    private Timestamp modifyDate;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
---
+++ src/main/java/com/example/vndreamers/models/Permission.java
@@ -0,0 +1,32 @@
+package com.example.vndreamers.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name = "permission", schema = "vndreamers")
+@Data
+public class Permission {
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Basic
+    @Column(name = "permission_name", nullable = false, length = 150)
+    private String permissionName;
+
+    @Basic
+    @Column(name = "created_date", nullable = false)
+    private Timestamp createdDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "role_permission",
+            joinColumns = @JoinColumn(name = "permission_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
