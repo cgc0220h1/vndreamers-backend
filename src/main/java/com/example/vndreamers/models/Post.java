899cc8d927191b4cce2af7408cac531258d51d36
Duc <vanduc2514@gmail.com>
2020-07-18 16:21:16 +0700

    Thêm Post và Relationship Entity

---
+++ src/main/java/com/example/vndreamers/models/Post.java
@@ -0,0 +1,50 @@
+package com.example.vndreamers.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name = "post", schema = "vndreamers")
+@Data
+public class Post {
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Basic
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Basic
+    @Column(name = "content", nullable = false, length = 100)
+    private String content;
+
+    @Basic
+    @Column(name = "share_link", nullable = false)
+    private String shareLink;
+
+    @Basic
+    @Column(name = "create_date", nullable = false)
+    private Timestamp createDate;
+
+    @Basic
+    @Column(name = "modify_date", nullable = false)
+    private Timestamp modifyDate;
+
+    @Basic
+    @Column(name = "status", nullable = false)
+    private int status;
+
+    @OneToMany(mappedBy = "post")
+    private Set<Comment> comments;
+
+    @OneToMany(mappedBy = "post")
+    private Set<LikePost> likes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User userByUserId;
+}
