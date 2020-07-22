eb282174b1b1c2c2606fc714732258d2e6d43a72
Duc <anhnam420@gmail.com>
2020-07-22 14:27:08 +0700

    Tách secret key và expireTIme ra file properties

--- src/main/java/com/codegym/vndreamers/services/auth/jwt/JWTIssuerImp.java
+++ src/main/java/com/codegym/vndreamers/services/auth/jwt/JWTIssuerImp.java
@@ -2,6 +2,7 @@ package com.codegym.vndreamers.services.auth.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -9,18 +10,23 @@ import java.util.Date;
 
 @Component
 public class JWTIssuerImp implements JWTIssuer {
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.expireTime}")
+    private Long expireTime;
 
     @Override
     public String generateToken(UserDetails userLogin) {
 
         Date loginTime = new Date();
         Date expiryTime = new Date();
-        expiryTime.setTime(loginTime.getTime() + 24 * 60 * 60);
+        expiryTime.setTime(loginTime.getTime() + expireTime);
         return Jwts.builder()
                 .setSubject(userLogin.getUsername())
                 .setIssuedAt(loginTime)
                 .setExpiration(expiryTime)
-                .signWith(SignatureAlgorithm.HS512, "vndreamers")
+                .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 
