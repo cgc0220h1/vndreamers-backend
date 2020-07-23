5e534f756295ef2fed950e6f199fc7ca19bef140
phi dao <daovanphi25051994@gmail.com>
2020-07-23 10:35:21 +0700

    Tạo controller save post

--- src/main/java/com/codegym/vndreamers/apis/PostAPI.java
+++ src/main/java/com/codegym/vndreamers/apis/PostAPI.java
@@ -1,6 +1,11 @@
 package com.codegym.vndreamers.apis;
 
+import com.codegym.vndreamers.exceptions.UserExistException;
+import com.codegym.vndreamers.models.Post;
+import com.codegym.vndreamers.models.User;
 import com.codegym.vndreamers.services.auth.jwt.JWTIssuer;
+import com.codegym.vndreamers.services.post.PostCRUDService;
+import com.codegym.vndreamers.services.user.UserCRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
@@ -8,10 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestController
 @RequestMapping("/api")
@@ -21,14 +29,23 @@ public class PostAPI {
     @Autowired
     private JWTIssuer tokenProvider;
 
-    @GetMapping("/posts")
-    public ResponseEntity<String> getAllPosts(HttpServletRequest request) {
+    @Autowired
+    private UserCRUDService userCRUDService;
+
+    @Autowired
+    private PostCRUDService postCRUDService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<String> getAllPosts(@RequestBody Post post, HttpServletRequest request) throws SQLIntegrityConstraintViolationException, UserExistException {
         String jwt = request.getHeader("Authorization");
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
             String username = tokenProvider.getUsernameFromJWT(jwt);
+            User user = userCRUDService.findByUsername(username);
+            post.setUser(user);
+            postCRUDService.save(post);
             return new ResponseEntity<>("ok", HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Token Invalid", HttpStatus.NOT_FOUND);
         }
     }
 }
