9dd97180bcfd276b0d999f8c4a135a890e8a2b34
phi dao <daovanphi25051994@gmail.com>
2020-07-23 08:26:06 +0700

    Tạo rest controller

---
+++ src/main/java/com/codegym/vndreamers/apis/PostAPI.java
@@ -0,0 +1,18 @@
+package com.codegym.vndreamers.apis;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class PostAPI {
+
+//    @GetMapping("/posts")
+////    public ResponseEntity<String> getAllPosts(){
+////        return new ResponseEntity<>()
+////    }
+}
