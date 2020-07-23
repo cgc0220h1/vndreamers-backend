d660c6dcff7303e4a0aace0697896a89ef4e6096
phi dao <daovanphi25051994@gmail.com>
2020-07-23 08:50:49 +0700

    Lấy header với tên là Authorization

--- src/main/java/com/codegym/vndreamers/apis/PostAPI.java
+++ src/main/java/com/codegym/vndreamers/apis/PostAPI.java
@@ -1,18 +1,24 @@
 package com.codegym.vndreamers.apis;
 
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class PostAPI {
 
-//    @GetMapping("/posts")
-////    public ResponseEntity<String> getAllPosts(){
-////        return new ResponseEntity<>()
-////    }
+    @GetMapping("/posts")
+    public ResponseEntity<String> getAllPosts(HttpServletRequest request ){
+        String bearerToken = request.getHeader("Authorization");
+        System.out.println(bearerToken);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
