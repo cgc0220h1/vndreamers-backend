25347014c6200a4fb59c92d3655ef6581860e6e8
phi dao <daovanphi25051994@gmail.com>
2020-07-23 09:31:11 +0700

    Lấy username từ Token

--- src/main/java/com/codegym/vndreamers/apis/PostAPI.java
+++ src/main/java/com/codegym/vndreamers/apis/PostAPI.java
@@ -1,8 +1,11 @@
 package com.codegym.vndreamers.apis;
 
+import com.codegym.vndreamers.services.auth.jwt.JWTIssuer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,17 @@ import javax.servlet.http.HttpServletRequest;
 @CrossOrigin("*")
 public class PostAPI {
 
+    @Autowired
+    private JWTIssuer tokenProvider;
+
     @GetMapping("/posts")
-    public ResponseEntity<String> getAllPosts(HttpServletRequest request ){
-        String bearerToken = request.getHeader("Authorization");
-        System.out.println(bearerToken);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> getAllPosts(HttpServletRequest request) {
+        String jwt = request.getHeader("Authorization");
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            String username = tokenProvider.getUsernameFromJWT(jwt);
+            return new ResponseEntity<>("ok", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
