dbd4d310a2c4e375959e150112d95f010406a84a
phi dao <daovanphi25051994@gmail.com>
2020-07-23 09:14:20 +0700

    Thêm hàm lấy username từ Token và hàm kiểm tra Token có hợp lệ không

--- src/main/java/com/codegym/vndreamers/services/auth/jwt/JWTIssuerImp.java
+++ src/main/java/com/codegym/vndreamers/services/auth/jwt/JWTIssuerImp.java
@@ -1,7 +1,12 @@
 package com.codegym.vndreamers.services.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -9,6 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JWTIssuerImp implements JWTIssuer {
 
     @Value("${jwt.secretKey}")
@@ -28,5 +34,29 @@ public class JWTIssuerImp implements JWTIssuer {
                 .setExpiration(expiryTime)
                 .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
     }
 }
