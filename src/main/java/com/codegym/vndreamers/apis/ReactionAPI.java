cf94b823564e760662a11796638174660e847e23
Duc <vanduc2514@gmail.com>
2020-07-27 02:10:43 +0700

    Thêm các trường consume để parse json từ request Thêm trường mặc định khởi tạo reaction là currenttime

--- src/main/java/com/codegym/vndreamers/apis/ReactionAPI.java
+++ src/main/java/com/codegym/vndreamers/apis/ReactionAPI.java
@@ -4,27 +4,21 @@ import com.codegym.vndreamers.exceptions.EntityExistException;
 import com.codegym.vndreamers.models.Post;
 import com.codegym.vndreamers.models.PostReaction;
 import com.codegym.vndreamers.models.User;
-import com.codegym.vndreamers.repositories.ReactionRepository;
 import com.codegym.vndreamers.services.post.PostCRUDService;
 import com.codegym.vndreamers.services.reaction.ReactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(
+        value = "/api",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE
+)
 @CrossOrigin("*")
 public class ReactionAPI {
 
