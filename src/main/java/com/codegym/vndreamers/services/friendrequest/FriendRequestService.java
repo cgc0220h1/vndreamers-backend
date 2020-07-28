f7bc826e495975e1c5418bef8a90f1a283a5027d
Duc <vanduc2514@gmail.com>
2020-07-28 23:13:14 +0700

    Fix Merge Code của Phi

--- src/main/java/com/codegym/vndreamers/services/friendrequest/FriendRequestService.java
+++ src/main/java/com/codegym/vndreamers/services/friendrequest/FriendRequestService.java
@@ -7,8 +7,13 @@ import java.util.List;
 
 public interface FriendRequestService extends GenericCRUDService<FriendRequest> {
     FriendRequest getFriendRequestByUserSensIdAndUserReceiveId(Integer userSendId, Integer userReceiveId);
-    List<FriendRequest> getAllFriendRequestByUserIdAndByStatus( Integer userId, int status);
+
+    List<FriendRequest> getAllFriendRequestByUserIdAndByStatus(Integer userId, int status);
+
     List<FriendRequest> getAllFriendRequestUserReceived(Integer userId, int status);
+
     List<FriendRequest> getAllFriendRequestSentByUser(Integer userId, int status);
+
+    boolean isFriend(Integer userSendId1, Integer userReceiveId2, int status);
 
 }
